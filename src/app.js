6c62b081966707dece3b307271e5fa78f79471ef
Gagnon, Marc <marc.gagnon1@cgi.com>
2019-10-30 17:00:06 -0400

    oups

---
+++ src/app.js
@@ -0,0 +1,81 @@
+#! /usr/bin/env node
+
+const promptMain = require('./promptMain');
+const promptGit = require('./promptGit');
+
+const cmdAngular = require('./cmdAngular');
+const cmdTheme = require('./cmdTheme');
+const cmdTesting = require('./cmdTesting');
+const cmdGit = require('./cmdGit');
+
+const helpers = require('./helpers');
+
+const prependFile = require('prepend-file');
+
+const fs = require('fs');
+const cp = require('child_process');
+
+async function main() {
+  const answers = await promptMain();
+
+  let gitName;
+  if (answers.git && answers.gitProvider === 'GitHub' && answers.gitNew) {
+    gitName = await promptGit.gitName(answers);
+    gitName = gitName.gitName;
+  }
+  let gitLink;
+  if (answers.git && answers.gitProvider === 'GitHub' && !answers.gitNew) {
+    gitLink = await promptGit.gitHub(answers.gitUsername);
+  }
+  if (answers.git && answers.gitProvider === 'GitLab' && !answers.gitNew) {
+    gitLink = await promptGit.gitLab(answers.gitUsername);
+  }
+  if (answers.git && answers.gitProvider === 'BitBucket' && !answers.gitNew) {
+    gitLink = await promptGit.bitbucket(answers.gitUsername);
+  }
+
+  const startTime = Date.now();
+  const toNull = process.platform === 'win32' ? 'nul 2>&1' : '/dev/null 2>&1';
+
+  process.chdir(answers.path);
+
+  cmdAngular(answers, toNull);
+  cmdTheme(answers, toNull);
+  cmdTesting(answers, toNull);
+
+  if (answers.hammer) {
+    helpers.printMsg('Installing HammerJS...');
+    if (answers.yarn) {
+      cp.execSync(`yarn install hammerjs > ${toNull}`);
+    } else {
+      cp.execSync(`npm install hammerjs > ${toNull}`);
+    }
+    prependFile('src/main.ts', "import 'hammerjs';", () => {});
+    helpers.printDone('Installing HammerJS...');
+  }
+
+  helpers.printMsg('Configuring TSLint...');
+  fs.readFile('tslint.json', (_, data) => {
+    data = tslint;
+    fs.writeFile('tslint.json', JSON.stringify(data, null, 2), () => {});
+  });
+  if (fs.existsSync('src/tslint.json')) {
+    fs.readFile('./src/tslint.json', (_, data) => {
+      data = tslintSrc;
+      fs.writeFile('src/tslint.json', JSON.stringify(data, null, 2), () => {});
+    });
+  }
+  helpers.printDone('Configuring TSLint...');
+
+  if (answers.git) {
+    cmdGit(answers, gitLink, toNull, gitName);
+  }
+
+  process.stdout.write(
+    `Done in ${(Math.round((Date.now() - startTime) / 1000) / 60).toFixed(
+      2,
+    )} mins`,
+  );
+}
+
+main();
---
+++ src/cmdAngular.js
@@ -0,0 +1,66 @@
+#!/usr/bin/env node
+
+const cp = require('child_process');
+const path = require('path');
+
+const helpers = require('./helpers');
+
+module.exports = function(answers, toNull) {
+  // Generating the command
+  let angularNewCommand = `ng new ${answers.name}`;
+  if (answers.routing) angularNewCommand += ` --routing=true`;
+  if (!answers.testing) angularNewCommand += ` --skipTests=true`;
+  if (!answers.framework) {
+    angularNewCommand += ` --style=scss`;
+  } else {
+    angularNewCommand += ` --style=${answers.framework}`;
+  }
+  if (!answers.git) {
+    angularNewCommand += ` --commit=false`;
+  }
+  angularNewCommand += ` --skipInstall=true`;
+
+  // Running the commands
+  if (answers.version === 'latest') {
+    helpers.printMsg('Updating Angular-CLI...');
+    if (answers.yarn) {
+      cp.execSync(`yarn global add @angular/cli@latest > ${toNull}`);
+    } else {
+      cp.execSync(`npm install -g @angular/cli@latest > ${toNull}`);
+    }
+    helpers.printDone('Updating Angular-CLI...');
+    helpers.printMsg('Creating the Angular App ...');
+    cp.execSync(`${angularNewCommand} > ${toNull}`);
+  } else {
+    let version;
+    try {
+      version = cp.execSync(`npx -version`).toString();
+    } catch (error) {
+      helpers.printMsg('Installing NPX...');
+      if (answers.yarn) {
+        cp.execSync(`yarn global add npx > ${toNull}`);
+      } else {
+        cp.execSync(`npm install -g npx > ${toNull}`);
+      }
+      helpers.printDone('Installing NPX...');
+    }
+    if (version) {
+      helpers.printMsg('Creating the Angular App ...');
+      if (answers.yarn) helpers.printWarning('Using NPX');
+      cp.execSync(
+        `npx -p @angular/cli@${answers.version} ${angularNewCommand} > ${toNull}`,
+      );
+    }
+  }
+
+  helpers.printDone('Creating the Angular App ...');
+  process.chdir(path.join(answers.path, answers.name));
+
+  helpers.printMsg('Installing Angular dependancies...');
+  if (answers.yarn) {
+    cp.execSync('yarn install');
+  } else {
+    cp.execSync('npm install');
+  }
+  helpers.printDone('Installing Angular dependancies...');
+};
